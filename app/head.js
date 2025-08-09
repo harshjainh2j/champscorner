b7f1f731ed0ffcf2253e4ffe9dc116ca801faf80
harshjainh2j <141403634+harshjainh2j@users.noreply.github.com>
2025-08-09 16:10:25 +0530

    gtag

--- app/head.js
+++ app/head.js
@@ -3,6 +3,22 @@ const Head = () => (
     <title>Champs Corner</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
+
+    {/* Google tag (gtag.js) */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=AW-757002568"
+    ></script>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-757002568');
+        `,
+      }}
+    />
   </>
 );
 
